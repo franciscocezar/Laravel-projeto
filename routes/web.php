1e14c2c824228808eefa7c926a602f9eaf9fd0b2
Francisco-Cezar <franciscocezar.jr@outlook.com>
2025-03-16 17:04:15 -0300

    adicionando todos os assets e criando as paginas do site de maneira estatica

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,15 @@ use App\Http\Controllers\Site\BlogController;
 use App\Http\Controllers\Site\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class);
+Route::get('/', HomeController::class)->name('site.home');
 
-Route::get('/produtos', [CategoryController::class, 'index']);
+Route::get('/produtos', [CategoryController::class, 'index'])->name('site.products');
 
-Route::get('/produtos/{slug}', [CategoryController::class, 'show']);
+Route::get('/produtos/{slug}', [CategoryController::class, 'show'])->name('site.produtcts.category');
 
-Route::get('/blog', BlogController::class);
+Route::get('/blog', BlogController::class)->name('site.blog');
 
-Route::view('sobre', 'site.about.index');
+Route::view('sobre', 'site.about.index')->name('site.about');
 
-Route::get('contato', [ContactController::class, 'index']);
-Route::post('contato', [ContactController::class, 'form']);
+Route::get('contato', [ContactController::class, 'index'])->name('site.contact');
+Route::post('contato', [ContactController::class, 'form'])->name('site.contact.form');
